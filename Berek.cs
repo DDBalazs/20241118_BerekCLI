3b033ec4c20cb4a5ea2a13e3ef369185a515bb8d
Balázs Zsolt Csőke <csoke.balazs@verebelyszki.hu>
2024-11-18 15:07:15 +0100

    Add project files.

---
+++ Berek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241118_BerekCLI
+{
+    internal class Berek
+    {
+        private string nev;
+        private string neme;
+        private string reszleg;
+        private int belepes;
+        private int ber;
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Neme { get => neme; set => neme = value; }
+        public string Reszleg { get => reszleg; set => reszleg = value; }
+        public int Belepes { get => belepes; set => belepes = value; }
+        public int Ber { get => ber; set => ber = value; }
+
+        public Berek(string sor)
+        {
+            string[] b = sor.Split(";");
+
+            Nev = b[0];
+            Neme = b[1];
+            Reszleg = b[2];
+            Belepes = int.Parse(b[3]);
+            Ber = int.Parse(b[4]);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using _20241118_BerekCLI;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+List<Berek> berek = [];
+
+using StreamReader sr = new("..\\..\\..\\src\\berek2020.txt",Encoding.UTF8);
+sr.ReadLine();
+while (!sr.EndOfStream) berek.Add(new(sr.ReadLine()));
+Console.WriteLine($"3.feladat: Dolgozók létszáma: {berek.Count}");
+
+var f4 = berek.Average(f => f.Ber);
+Console.WriteLine($"4.feladat: Bérek átlaga {f4:0.00} eFt");
+
+//f6
+Console.Write("5.feladat: Kérem a részleg nevét: ");
+var f5 = Console.ReadLine();
+
+Console.WriteLine("A legtöbbet kereső dolgozó a megadott részlegen: ");
+
+for(int i = 0; i<berek.Count; i++)
+{
+    if (berek[i].Reszleg == f5.ToLower())
+    {
+        var f6 = berek.Where(f => f.Reszleg == f5.ToLower()).Max(f => f.Ber);
+        Console.WriteLine($"Név: {berek[i].Nev}\nNem:{berek[i].Neme}\nBelépés: {berek[i].Belepes}\nBér: {berek[i].Ber} Forint");
+        return;
+    }
+}
+
+Console.WriteLine("7. Feladat: Statisztika");
+var f7 = berek.Select(f => f.Reszleg);
+foreach(var position in berek.Distinct())
+{
+    Console.WriteLine($"\n{position} - {berek.Count(f => f.Reszleg.Equals(position))} fő;");
+}
